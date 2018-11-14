5ae645d7a0ac90c72e0d993b4336072f5b789f82
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-11-14 07:49:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+speakers = ["Edsger","Ada","Charles","Alan","Grace", "Linus", "Matz"]
+
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  batch_badges = []
+  array.each do |name|
+    batch_badges << "Hello, my name is #{name}."
+  end
+  return batch_badges
+end
+
+
+
+
+    
